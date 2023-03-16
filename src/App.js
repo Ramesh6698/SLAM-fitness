0ca96796766866643701aec25841466fea725e48
Ramesh6698 <rameshsekar6698@gmail.com>
2023-03-16 14:01:02 +0530

    First Project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "React";
+//npm install font -awesome --save
+
+class Navbar extends React.Component{
+render(){
+return(
+
+  <div className="navbaritems">
+    <h1 className="navbar-logo"><i className="fab fa-react"> Logo </i> </h1>
+
+
+
+  </div>
+)
+
+
+
+}
+
+
+
+
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import ReactDom from "react-dom";
+import NavBar from "./App";
+
+function App(){
+retur(
+  < navbar/>
+)
+
+
+}
+
+
+ReactDom.render(<app/>,document.getElemtById('root'));
